0cc0519d56eb694d45a137265ad1f2145a7d84ac
avrahman <avarahman.wp@gmail.com>
2025-05-07 14:21:06 +0200

    Added: 'publication date 'author name', 'post tags' to includes/section-content

--- includes/section-content.php
+++ includes/section-content.php
@@ -8,8 +8,25 @@
                         <article>
                             <h1> <?php the_title(); ?> </h1>
                             <?php the_post_thumbnail('medium'); ?>
-                            <?php the_content() ?>
+                            <p> Iffghed ass n <?php echo get_the_date('d/m/Y h:i:s'); ?> </p>
+                            <?php the_content(); ?>
+                            <?php // the_author(); 
+                            ?>
+                            <?php $fname = get_the_author_meta('first_name'); ?>
+                            <?php $lname = get_the_author_meta('last_name'); ?>
+                            <p> Yura-t-id <?php echo $fname . ' ' . $lname; ?> </p>
                         </article>
+                        <section>
+                            <?php
+                            $tags = get_the_tags();
+                            foreach ($tags as $tag): ?>
+                                <a href="<?php echo get_tag_link($tag->term_id);?>"> 
+                                    <?php echo $tag->name; ?>
+                                </a>
+                            <?php
+                            endforeach;
+                            ?>
+                        </section>
                     <?php
                     endwhile;
                 else:
