7ee5c9ce42a548bd00b76d175da32d45ad12703d
avrahman <avarahman.wp@gmail.com>
2025-05-15 20:04:02 +0200

    Creation php files : 'archive-cars', 'includes/section-cars', 'single-cars'

---
+++ single-cars.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+    <main class="container">
+
+        <section class="page-wrap">
+            <h1>single.php</h1>
+
+            <?php get_template_part('includes/section', 'cars'); ?>
+
+            <?php wp_link_pages(); ?>
+        </section>
+
+        <?php if (is_active_sidebar("articles")): ?>
+            <aside>
+                <?php dynamic_sidebar("articles"); ?>
+            </aside>
+        <?php else: ?>
+            <p> Pas de sidebar !</p>
+        <?php endif; ?>
+
+    </main>
+
+<?php get_footer(); ?>
