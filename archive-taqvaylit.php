1f5e06d9a369a0fe1cb1adceda0fa6bb3e695f02
avrahman <avarahman.wp@gmail.com>
2025-05-17 22:43:29 +0200

    [200~archive-taqvaylit.php         new file:   category-taqvaylit.php         new file:   includes/taqvaylit-archive.php  'single-tutlayt'

---
+++ archive-taqvaylit.php
@@ -0,0 +1,40 @@
+<?php get_header("about"); ?>
+
+<main class="container">
+
+    <section class="row">
+
+        <div class="col-lg-3">
+            <?php if (is_active_sidebar("articles")): ?>
+                <?php dynamic_sidebar('articles'); ?>
+            <?php endif; ?>
+        </div>
+
+        <div class="col-lg-9">
+            <h1> Nom de la catégorie: <?php echo single_cat_title(); ?> </h1> <!-- DISPLAYS THE TITLE OF THE CATEGORY -->
+
+            <?php get_template_part('includes/section', 'archive'); ?>
+
+            <?php
+            global $wp_query;
+            $big = 999999999;
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                'format' => '?paged=%#%',
+                'current' => max(1, get_query_var('paged')),
+                'total' => $wp_query->max_num_pages
+            ));
+            ?>
+
+            <!-- OR: the code bellow -->
+            <?php // previous_posts_link(); // Added in'section-archive.php' template part 
+            ?>
+            <?php // next_posts_link(); // Added in'section-archive.php' template part 
+            ?>
+        </div>
+
+    </section>
+
+</main>
+
+<?php get_footer(); ?>
---
+++ category-taqvaylit.php
@@ -0,0 +1,36 @@
+<?php get_header("about"); ?>
+
+<main class="container">
+    <section class="page-wrap">
+
+    <h1> Nom de la catég orie: <?php echo single_cat_title(); ?> </h1> <!-- DISPLAYS THE TITLE OF THE CATEGORY -->
+
+    <?php get_template_part('includes/taqvaylit', 'archive'); ?>
+
+    <?php previous_posts_link(); // Added in'section-archive.php' template part ?>
+    <?php next_posts_link(); // Added in'section-archive.php' template part ?>
+
+    <!-- OR: the code down -->
+    <?php /*
+        global $wp_query;
+        $big = 999999999;
+        echo paginate_links(array(
+            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $wp_query->max_num_pages
+        )); */
+    ?>
+
+    </section>
+    <?php if (is_active_sidebar("articles")): ?>
+        <aside>
+            <?php dynamic_sidebar('articles'); ?>
+        </aside>
+    <?php else: ?>
+        <p>No sidebar !</p>
+    <?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?>
